c38521afe816bfa58da386cb8cb737f508511866
Angel_Aguilera <albertoangelaguilera@gmail.com>
2018-10-26 13:00:22 -0500

    se modifica la pagina de contacto

--- src/components/Contactanos.vue
+++ src/components/Contactanos.vue
@@ -1,48 +1,115 @@
 <template>
   <div page_container-contacto>
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-    <div class="info-redes">
-      <p> ¡Conecta con nuestras redes sociales! </p>
-<md-icon class="fab fa-facebook"></md-icon>
-<md-icon class="fab fa-twitter"></md-icon>
-<p>¿Prefieres contactarnos por telefono?</p>
-<div class="tel">
-<table id="telefono">
-    <tr>
-        <th>Gerencia</th>
-        <th>Ventas</th>
-    </tr>
-    <tr>
-        <th id="telefono-icono"><md-icon class="fas fa-phone"> <span id="tel"><b>01(993) 353 0555</b></span> </md-icon></th>
-        <th id="telefono-icono"><md-icon class="fas fa-phone"> <span id="tel"><b>01(922) 113 4494</b></span> </md-icon></th>
-    </tr>
-    <tr>
-        <th id="whats-icono"><md-icon class="fab fa-whatsapp"> <span id="tel"><b>993 120 7440</b></span> </md-icon></th>
-        <th id="whats-icono"><md-icon class="fab fa-whatsapp"> <span id="tel"><b>992 113 4494</b></span> </md-icon></th>
-    </tr>
-</table>
-</div>
-    </div>
-    <md-field>
-      <md-icon>email</md-icon>
-      <label><b>Tu correo electronico</b> </label>
-      <md-input v-model="correo"></md-input>
-    </md-field>
 
-    <md-field>
-              <md-icon>person</md-icon>
+         <head>
+	<title>Contact V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <label><b>Nombre</b></label>
-      <md-input v-model="nombre"></md-input>
-    </md-field>
-<div class="mensaje">
-   <md-field>
-      <md-icon>message</md-icon>
-      <label><b>Mensaje</b></label>
-      <md-textarea v-model="mensaje"></md-textarea>
-    </md-field>
-</div>
-      <md-button class="md-raised md-accent"><b>Enviar</b></md-button>
+<!--===============================================================================================-->
+</head>
+<body>
+
+	<div class="contact1">
+		<div class="container-contact1">
+			<div class="contact1-pic js-tilt" data-tilt>
+				<i class="fab fa-facebook" alt="IMG"></i>
+                            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+            HOI CRAYOLIL
+
+			</div>
+
+			<form class="contact1-form validate-form">
+				<span class="contact1-form-title">
+                    ¡Mándanos un mensaje!
+				</span>
+
+				<div class="wrap-input1 validate-input" data-validate = "Se requiere nombre">
+					<input class="input1" type="text" name="name" placeholder="Nombre">
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate = "Correo valido es requerido: ejemplo@abc.xyz">
+					<input class="input1" type="text" name="email" placeholder="Email">
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate = "Es necesario el Asunto">
+					<input class="input1" type="text" name="subject" placeholder="Asunto">
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate = "Se requiere un cuerpo en el mensaje">
+					<textarea class="input1" name="message" placeholder="Mensaje"></textarea>
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="container-contact1-form-btn">
+					<button class="contact1-form-btn">
+						<span>
+							Enviar
+							<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+
+</body>
+
 </div>
   
 </template>
@@ -58,81 +125,412 @@
   }
 </script>
 
-<style lang="scss" scoped>
-#telefono{
+<style lang="css" scoped>
+
+
+
+
+/*//////////////////////////////////////////////////////////////////
+[ RESTYLE TAG ]*/
+.contact1-pic {
+    border-style: groove;
+  }
+* {
+	margin: 0px; 
+	padding: 0px; 
+	box-sizing: border-box;
+}
+
+body, html {
+	height: 100%;
+	font-family: Montserrat-Regular, sans-serif;
+}
+
+/*---------------------------------------------*/
+a {
+	font-size: 14px;
+	line-height: 1.7;
+	color: #666666;
+	margin: 0px;
+	transition: all 0.4s;
+	-webkit-transition: all 0.4s;
+  -o-transition: all 0.4s;
+  -moz-transition: all 0.4s;
+}
+
+a:focus {
+	outline: none !important;
+}
+
+a:hover {
+	text-decoration: none;
+	color: #57b846;
+}
+
+/*---------------------------------------------*/
+h1,h2,h3,h4,h5,h6 {
+	margin: 0px;
+}
+
+p {
+	font-size: 14px;
+	line-height: 1.7;
+	color: #666666;
+	margin: 0px;
+}
+
+ul, li {
+	margin: 0px;
+	list-style-type: none;
+}
+
+
+/*---------------------------------------------*/
+input {
+	outline: none;
+	border: none;
+}
+
+textarea {
+  outline: none;
+  border: none;
+}
+
+textarea:focus, input:focus {
+  border-color: transparent !important;
+}
+
+input::-webkit-input-placeholder { color: #999999; }
+input:-moz-placeholder { color: #999999; }
+input::-moz-placeholder { color: #999999; }
+input:-ms-input-placeholder { color: #999999; }
+
+textarea::-webkit-input-placeholder { color: #999999; }
+textarea:-moz-placeholder { color: #999999; }
+textarea::-moz-placeholder { color: #999999; }
+textarea:-ms-input-placeholder { color: #999999; }
+
+/*---------------------------------------------*/
+button {
+	outline: none !important;
+	border: none;
+	background: transparent;
+}
+
+button:hover {
+	cursor: pointer;
+}
+
+iframe {
+	border: none !important;
+}
+
+
+
+
+/*//////////////////////////////////////////////////////////////////
+[ Contact 1 ]*/
+
+.contact1 {
+  width: 100%;
+  min-height: 100%;
+  padding: 15px;
+
+  background: #198C19;
+  background: -webkit-linear-gradient(left, #198C19, #32cd32);
+  background: -o-linear-gradient(left, #198C19, #32cd32);
+  background: -moz-linear-gradient(left, #198C19,#32cd32);
+  background: linear-gradient(left, #198C19,#32cd32);
+
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+
+.container-contact1 {
+  width: 1163px;
+  background: #fff;
+  border-radius: 10px;
+  overflow: hidden;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 90px 130px 88px 148px;
+}
+
+/*------------------------------------------------------------------
+[  ]*/
+.contact1-pic {
+  width: 296px;
+}
+
+.contact1-pic img {
+  max-width: 100%;
+}
+.contact1-pic{
+    border-style: groove;
+}
+
+/*------------------------------------------------------------------
+[  ]*/
+.contact1-form {
+  width: 390px;
+}
+
+.contact1-form-title {
+  display: block;
+  font-size: 24px;
+  color: #333333;
+  line-height: 1.2;
+  text-align: center;
+  padding-bottom: 44px;
+}
+
+input.input1 {
+  height: 50px;
+  border-radius: 25px;
+  padding: 0 30px;
+}
+input.input1 + .shadow-input1 {
+  border-radius: 25px;
+}
+
+textarea.input1 {
+  min-height: 150px;
+  border-radius: 25px;
+  padding: 12px 30px;
+}
+textarea.input1 + .shadow-input1 {
+  border-radius: 25px;
+}
+
+/*---------------------------------------------*/
+.wrap-input1 {
+  position: relative;
+  width: 100%;
+  z-index: 1;
+  margin-bottom: 20px;
+}
+
+.input1 {
+  display: block;
+  width: 100%;
+  background: #e6e6e6;
+  font-size: 15px;
+  line-height: 1.5;
+  color: #666666;
+}
+
+.shadow-input1 {
+  content: '';
+  display: block;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  box-shadow: 0px 0px 0px 0px;
+  color: rgba(87,184,70, 0.5);
+}
+
+.input1:focus + .shadow-input1 {
+  -webkit-animation: anim-shadow 0.5s ease-in-out forwards;
+  animation: anim-shadow 0.5s ease-in-out forwards;
+}
+
+@-webkit-keyframes anim-shadow {
+  to {
+    box-shadow: 0px 0px 80px 30px;
+    opacity: 0;
+  }
+}
+
+@keyframes anim-shadow {
+  to {
+    box-shadow: 0px 0px 80px 30px;
+    opacity: 0;
+  }
+}
+
+/*---------------------------------------------*/
+.container-contact1-form-btn {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.contact1-form-btn {
+  min-width: 193px;
+  height: 50px;
+  border-radius: 25px;
+  background: #57b846;
+  font-size: 15px;
+  line-height: 1.5;
+  color: #fff;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 25px;
+
+  -webkit-transition: all 0.4s;
+  -o-transition: all 0.4s;
+  -moz-transition: all 0.4s;
+  transition: all 0.4s;
+}
+
+.contact1-form-btn i {
+  margin-left: 7px;
+
+  -webkit-transition: all 0.4s;
+  -o-transition: all 0.4s;
+  -moz-transition: all 0.4s;
+  transition: all 0.4s;
+}
+
+.contact1-form-btn:hover {
+  background: #333333;
+}
+
+.contact1-form-btn:hover i {
+  -webkit-transform: translateX(10px);
+  -moz-transform: translateX(10px);
+  -ms-transform: translateX(10px);
+  -o-transform: translateX(10px);
+  transform: translateX(10px);
+}
+
+
+
+
+/*------------------------------------------------------------------
+[ Responsive ]*/
+
+@media (max-width: 1200px) {
+  .contact1-pic {
+    width: 33.5%;
+  }
+
+  .contact1-form {
+    width: 44%;
+  }
+}
+
+@media (max-width: 992px) {
+  .container-contact1 {
+    padding: 90px 80px 88px 90px;
+  }
+
+  .contact1-pic {
+    width: 35%;
+  }
+
+  .contact1-form {
+    width: 55%;
+  }
+}
+
+@media (max-width: 768px) {
+  .container-contact1 {
+    padding: 90px 80px 88px 80px;
+  }
+
+  .contact1-pic {
+    display: none;
+  }
+
+  .contact1-form {
     width: 100%;
-    border:0;
-    position: relative;
-    right: -4.5em;
-
-}
-#telefono-icono{
-    position: relative;
-    right: -1.5em; 
-}
-#whats-icono{
-    position: relative;
-    right: -4em;
-    padding: 0;
-    top: -1.5em;
-}
-#gerencia{
-    position: relative;
-    top: -18px;
-    color:#198C19;
-}
-#ventas{
-      position: relative;
-    top: -18px;
-        right: -15px;
-
-    color:#198C19;
-}
-.info-redes{
-  font-family: 'Quicksand', sans-serif;
-
-}
-.fa-facebook{
-    color:blue
-}
-.fa-twitter{
-    color:aqua
-}
-.fa-phone{
-position: relative;
-right: 3.5em;
-}
-.fa-whatsapp{
-position: relative;
-top: 1em;
-right: 4.5em;
-}
-.tel{
-    position: relative;
-    right: 4.5em;
-}
-#tel{
-font-family: 'Quicksand', sans-serif;
-font-size: .6em;
-
-}
-
-.md-field{
-      font-family: 'Quicksand', sans-serif;
-      padding: 35px;
-}
-.mensaje{
-    border-style: double;
-    width:100%;
-}
-  .md-field:last-child {
-    margin-bottom: 40px;
   }
-  .md-button{
-      background-color: #198C19;
-      color:white;
+}
+
+@media (max-width: 576px) {
+  .container-contact1 {
+    padding: 90px 15px 88px 15px;
   }
-  i{
-      color: #198C19
+}
+
+
+/*------------------------------------------------------------------
+[ Alert validate ]*/
+
+.validate-input {
+  position: relative;
+}
+
+.alert-validate::before {
+  content: attr(data-validate);
+  position: absolute;
+  max-width: 70%;
+  background-color: white;
+  border: 1px solid #c80000;
+  border-radius: 13px;
+  padding: 4px 25px 4px 10px;
+  top: 50%;
+  -webkit-transform: translateY(-50%);
+  -moz-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  -o-transform: translateY(-50%);
+  transform: translateY(-50%);
+  right: 8px;
+  pointer-events: none;
+  color: #c80000;
+  font-size: 13px;
+  line-height: 1.4;
+  text-align: left;
+
+  visibility: hidden;
+  opacity: 0;
+
+  -webkit-transition: opacity 0.4s;
+  -o-transition: opacity 0.4s;
+  -moz-transition: opacity 0.4s;
+  transition: opacity 0.4s;
+}
+
+.alert-validate::after {
+  content: "\f06a";
+  display: block;
+  position: absolute;
+  color: #c80000;
+  font-size: 15px;
+  top: 50%;
+  -webkit-transform: translateY(-50%);
+  -moz-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  -o-transform: translateY(-50%);
+  transform: translateY(-50%);
+  right: 13px;
+}
+
+.alert-validate:hover:before {
+  visibility: visible;
+  opacity: 1;
+}
+
+@media (max-width: 992px) {
+  .alert-validate::before {
+    visibility: visible;
+    opacity: 1;
   }
+}
 </style>
